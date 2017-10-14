61f69a51feb848205b86652bec6f2eabaddec5e7
ehco1996 <zh19960202@gmail.com>
2017-10-14 08:53:06 +0800

    前端基础10

---
+++ learnjs/lesson10/课程15预习.js
@@ -0,0 +1,37 @@
+// 2016/10/18
+//
+// 此为第 15 课的上课内容
+//
+// 今天上课的主要内容有
+//
+// log 的要素
+// git 和 github
+// js 的一些高级内容 bind apply call
+// 前端 CSS 框架 Bootstrap 和 Pure
+//
+
+
+/*
+log 的要素
+  1, 确保代码执行
+  2, 打印所有能打印的变量
+
+
+github 网站使用方法参考如下链接
+https://www.zhihu.com/question/20070065/answer/79557687
+
+
+这是两个比较流行的 CSS 框架(别人帮你写好你直接拿来就可以用的 CSS 文件)
+
+Bootstrap 官网如下
+http://getbootstrap.com/
+简易教程如下(先随便看看)
+http://www.runoob.com/bootstrap/bootstrap-tutorial.html
+
+Pure 官网如下
+http://purecss.io/
+
+
+js 的东西得上课配合实例讲解
+
+*/
